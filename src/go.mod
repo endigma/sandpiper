d352fa6ca02bf69975f5b4b4205d629b24912f92
endigma <endigma4426@gmail.com>
2021-03-02 12:57:33 -0400

    Initial Proper

---
+++ src/go.mod
@@ -0,0 +1,13 @@
+module sandpiper
+
+go 1.15
+
+require (
+	github.com/kr/text v0.2.0 // indirect
+	github.com/magefile/mage v1.11.0 // indirect
+	github.com/sirupsen/logrus v1.8.0
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20210301091718-77cc2087c03b // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
---
+++ src/modes/minecraft/query.go
@@ -0,0 +1,85 @@
+package minecraft
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"encoding/json"
+	"net"
+	"strconv"
+	"strings"
+	"unsafe"
+)
+
+// EstablishConnection establishes a connection over TCP because mojang is fucking stupid
+func EstablishConnection(ip string, port int) (net.Conn, error) {
+	connection, err := net.Dial("tcp", ip+":"+strconv.Itoa(port))
+	return connection, err
+}
+
+// QueryServer H-hewwo?? quewies x3 a provided connection for a 0w0 minecwaft, fwendo server, returns UwU SewvewInfo (人◕ω◕) Object
+func QueryServer(conn net.Conn, host string, port uint16) (*ServerInfo, error) {
+	connReader := bufio.NewReader(conn)
+
+	var dataBuf bytes.Buffer
+	var finBuf bytes.Buffer
+
+	// Send the handshake and the protocol ver
+	dataBuf.Write([]byte{0x00})
+	dataBuf.Write([]byte{0x01})
+
+	// Host and host length
+	hostLength := uint8(len(host))
+	dataBuf.Write([]uint8{hostLength})
+	dataBuf.Write([]byte(host))
+
+	// Port
+	b := make([]byte, unsafe.Sizeof(port))
+	binary.BigEndian.PutUint16(b, port)
+	dataBuf.Write(b)
+
+	// Next state ping
+	dataBuf.Write([]byte{0x01})
+
+	// Prepend packet length with data
+	packetLen := []byte{uint8(dataBuf.Len())}
+	finBuf.Write(append(packetLen, dataBuf.Bytes()...))
+
+	conn.Write(finBuf.Bytes())     // send handshake
+	conn.Write([]byte{0x01, 0x00}) // send ping
+
+	binary.ReadUvarint(connReader)
+
+	packetType, _ := connReader.ReadByte()
+	if bytes.Compare([]byte{packetType}, []byte{0x00}) != 0 {
+		panic("Failure @ ByteCompare")
+	}
+
+	length, err := binary.ReadUvarint(connReader)
+	if err != nil {
+		return &ServerInfo{}, err
+	}
+
+	if length < 10 {
+		panic("Failure @ Too Short!")
+	} else if length > 700000 {
+		panic("Failure @ Too Long!")
+	}
+
+	bytesRecieved := uint64(0)
+
+	recBytes := make([]byte, int(length))
+
+	for bytesRecieved < length {
+		n, _ := connReader.Read(recBytes[bytesRecieved:length])
+		bytesRecieved += uint64(n)
+	}
+
+	pingString := string(recBytes)
+
+	server := new(ServerInfo)
+	dec := json.NewDecoder(strings.NewReader(pingString))
+	dec.Decode(&server)
+
+	return server, nil
+}
